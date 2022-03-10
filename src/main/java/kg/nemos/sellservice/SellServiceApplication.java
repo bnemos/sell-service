c56cbacefaee29bb13a4c3d35e0b770583cbffb8
Namys <bnamys16@gmail.com>
2022-03-10 16:41:19 +0600

    Initial commit

---
+++ src/main/java/kg/nemos/sellservice/SellServiceApplication.java
@@ -0,0 +1,12 @@
+package kg.nemos.sellservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellServiceApplication.class, args);
+    }
+}
